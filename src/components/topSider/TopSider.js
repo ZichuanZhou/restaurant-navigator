d94bc44f472a52e30707e6326418331e9e552a65
zichuan.zhou <zichuan.zhou@ansibleww.com>
2018-10-28 16:47:52 +1030

    css modification for responsive design

--- src/components/topSider/TopSider.js
+++ src/components/topSider/TopSider.js
@@ -118,7 +118,7 @@ export default class TopSider extends Component {
           </div>
         </div>
 
-        <div className="inline-block3 right">
+        <div className="inline-block3">
           <div className="grid-container1">
             <MySlider
               filterRestaurant={this.props.filterRestaurant}
